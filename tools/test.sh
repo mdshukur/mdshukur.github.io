7db7a51a7e899237b6008fb7de3f7dc615faa689
mdshukur <mdshukurali222333@gmail.com>
2026-05-29 20:29:15 +0600

    Ignore HTTP links in htmlproofer

--- tools/test.sh
+++ tools/test.sh
@@ -62,8 +62,9 @@ main() {
 
   # test
   bundle exec htmlproofer "$SITE_DIR" \
-    --disable-external \
-    --ignore-urls "/^http:\/\/127.0.0.1/,/^http:\/\/0.0.0.0/,/^http:\/\/localhost/"
+  --disable-external \
+  --ignore-urls "/^http:\/\/127.0.0.1/,/^http:\/\/0.0.0.0/,/^http:\/\/localhost/,/^http:\/\//"
+
 }
 
 while (($#)); do
